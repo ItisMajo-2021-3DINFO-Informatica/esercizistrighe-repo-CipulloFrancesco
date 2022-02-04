0b344aa97d69b63e238813709e68fe634481bd3b
CipulloFrancesco <98150895+CipulloFrancesco@users.noreply.github.com>
2022-02-04 10:46:57 +0100

    Primo esercizio strighe

---
+++ CipulloEserciziStringheApp1/CipulloEserciziStringheApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CipulloEserciziStringheApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Inserisci una frase ");
+            string frase = Console.ReadLine();
+
+            int N = 0;
+            int contaVocali=0;
+            int contaConsonanti = 0;
+            int i = 0;
+            while (i < N)
+            {
+                Console.WriteLine(" Inserisci una parola ");
+                string parola = Console.ReadLine();
+
+                char primoCarattere = parola[0];
+
+                if (primoCarattere != 'a')
+
+                {
+                    contaVocali++;
+                }
+                else if (primoCarattere != 'e')
+
+                {
+                    contaVocali++;
+                }
+
+                else if (primoCarattere != 'i')
+
+                {
+                    contaVocali++;
+                }
+
+                else if (primoCarattere != 'o')
+
+                {
+                    contaVocali++;
+                }
+
+                else if (primoCarattere != 'u')
+
+                {
+                    contaVocali++;
+                }
+
+                else
+                {
+                    contaConsonanti++;
+                }
+
+                i++;
+
+              }
+
+                  Console.WriteLine("il numero di parole che iniziano con vocale sono" + contaVocali);
+                  Console.WriteLine("il numero di parole che iniziano con consonante sono" + contaConsonanti);
+          }
+        }
+    }
+}
